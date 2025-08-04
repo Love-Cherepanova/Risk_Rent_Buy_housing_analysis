2fd90f866f8d4d2aa1cfc8bc3ebcc0b921d44691
Love-Cherepanova <cherepanovalove0@gmail.com>
2025-08-04 09:26:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class InputData(BaseModel):
+  price: float
+  rent: float
+  risk: float
+
+@app.post("/predict')
+def predict(data: InputData):
+  score = data.price / (data.rent + 1) - data.risk
+  return {"score": score}
+
+
+
+
+#Add FastAPI main.py
+      
